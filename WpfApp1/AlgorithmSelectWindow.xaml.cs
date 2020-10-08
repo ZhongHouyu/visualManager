dc027fb5dad310855c01eaca3d47ae09d8b9e3b2
ZhongHouyu <993837013@qq.com>
2020-10-08 17:17:13 +0800

    add minist-test

---
+++ WpfApp1/AlgorithmSelectWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// AlgorithmSelectWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class AlgorithmSelectWindow : Window
+    {
+        public AlgorithmSelectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            reshape_grid.Visibility = System.Windows.Visibility.Visible;
+
+
+
+        }
+
+        private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            MINIST_grid.Visibility = System.Windows.Visibility.Visible;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MINIST_Tester mINIST_Tester = new MINIST_Tester();
+            double[] score = mINIST_Tester.MNIST_test();
+            
+            MessageBox.Show($"{score[0]}");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string p = "D://data//test//data.txt";
+            FileStream fs = new FileStream(p, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            //开始写入
+            decimal c = decimal.Parse(width.Text);
+            int w = (int)c;
+            c = decimal.Parse(lenth.Text);
+            int l = (int)c;
+            sw.Write(w);
+            sw.Write('\n');
+            sw.Write(l);
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+            this.Close();
+
+        }
+
+    }
+}
---
+++ WpfApp1/MINIST_Tester.cs
@@ -0,0 +1,97 @@
+﻿using Keras;
+using Keras.Datasets;
+using Keras.Layers;
+using Keras.Models;
+using Keras.Optimizers;
+using Keras.Utils;
+using Numpy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tensorflow.Keras.Datasets;
+
+namespace WpfApp1
+{
+    class MINIST_Tester
+    {
+        private int random_state;
+        private double test_size;
+
+        public double[] MNIST_test()
+        {
+            int batch_size = 128;
+            int num_classes = 10;
+            int epochs = 12;
+
+            // input image dimensions
+            int img_rows = 28, img_cols = 28;
+
+            Shape input_shape = null;
+
+            // the data, split between train and test sets
+            var ((x_train, y_train), (x_test, y_test)) = MNIST.LoadData();
+
+            if (Backend.ImageDataFormat() == "channels_first")
+            {
+                x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols);
+                x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols);
+                input_shape = (1, img_rows, img_cols);
+            }
+            else
+            {
+                x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1);
+                x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1);
+                input_shape = (img_rows, img_cols, 1);
+            }
+
+            x_train = x_train.astype(np.float32);
+            x_test = x_test.astype(np.float32);
+            x_train /= 255;
+            x_test /= 255;
+            Console.WriteLine($"x_train shape: {x_train.shape}");
+            Console.WriteLine($"{x_train.shape[0]} train samples");
+            Console.WriteLine($"{x_test.shape[0]} test samples");
+
+            // convert class vectors to binary class matrices
+            y_train = Util.ToCategorical(y_train, num_classes);
+            y_test = Util.ToCategorical(y_test, num_classes);
+
+            // Build CNN model
+            var model = new Sequential();
+            model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+                                    activation: "relu",
+                                    input_shape: input_shape));
+            model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+            model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+            model.Add(new Dropout(0.25));
+            model.Add(new Flatten());
+            model.Add(new Dense(128, activation: "relu"));
+            model.Add(new Dropout(0.5));
+            model.Add(new Dense(num_classes, activation: "softmax"));
+
+            model.Compile(loss: "categorical_crossentropy",
+                optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
+
+            model.Fit(x_train, y_train,
+                        batch_size: batch_size,
+                        epochs: epochs,
+                        verbose: 1,
+                        validation_data: new NDarray[] { x_test, y_test });
+            var score = model.Evaluate(x_test, y_test, verbose: 0);
+            Console.WriteLine($"Test loss: {score[0]}");
+            Console.WriteLine($"Test accuracy: {score[1]}");
+
+
+            return score;
+        }
+
+        private object train_test_split(object x, object y, object p1, object p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+        
